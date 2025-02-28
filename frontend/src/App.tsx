2ad4ec4d59adc130db43004d346d2d6223cd30a8
Karan-Codes-Hub <karankantm@gmail.com>
2025-02-28 23:26:51 +0530

    added  frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-2xl">
+      Hello, Tailwind + React + TypeScript  new project 
+    </div>
+  );
+};
+
+
+export default App
